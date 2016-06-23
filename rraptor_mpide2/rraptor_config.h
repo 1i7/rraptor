c5f1a19259109491b5b339e9766a671385f6dfef
1i7 <ii1i7@yandex.ru>
2016-06-23 18:49:52 +0300

    Увеличим номер версии в связи с переходом на модульность

--- rraptor_mpide2/rraptor_config.h
+++ rraptor_mpide2/rraptor_config.h
@@ -5,7 +5,7 @@ static const char* DEVICE_NAME = "Anton's Rraptor";
 static const char* DEVICE_MODEL = "Rraptor";
 static const char* DEVICE_SERIAL_NUMBER = "00000003";
 static const char* DEVICE_DESCRIPTION = "Multifunctional CNC unit";
-static const char* DEVICE_VERSION = "0.2-devel";
+static const char* DEVICE_VERSION = "0.3-modular-devel";
 static const char* DEVICE_MANUFACTURER = "Rraptor team";
 static const char* DEVICE_URI = "http://rraptor.lasto4ka.su";
 
